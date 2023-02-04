c0a8175954fc4b64a00aa4fdea224924160e27fc
zainahmed0597 <zainahmed0597@gmail.com>
2023-02-05 01:33:24 +0500

    Basic setup app

---
+++ lib/app/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  // const MyApp({super.key}); // default constructor
+
+  MyApp._internal(); // private named constructor
+  int appState = 0;
+  static final MyApp instance =
+      MyApp._internal(); // single instance -- singleton
+
+  factory MyApp() => instance; // factory for class instance
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:project_1/app/app.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  void updateAppState() {
+    MyApp.instance.appState = 10;
+  }
+
+  void getAppState() {
+    print(MyApp.instance.appState); // 10
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
