d6d7e73502ec7ac05a2eaee184c2a68a30f19c94
zainahmed0597 <zainahmed0597@gmail.com>
2023-02-13 05:01:09 +0500

    create dummy data api and add response file

---
+++ lib/data/responses.dart
@@ -0,0 +1,45 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: "status")
+  int? status;
+  @JsonKey(name: "massage")
+  String? massage;
+}
+
+@JsonSerializable()
+class CustomerResponse{
+  @JsonKey(name: "id")
+  String? id;
+  @JsonKey(name: "name")
+  String? name;
+  @JsonKey(name: "numOfNotifications")
+  int? numbOfNotifications;
+
+  CustomerResponse(this.id, this.name, this.numbOfNotifications);
+}
+
+@JsonSerializable()
+class ContactsResponse{
+  @JsonKey(name: "phone")
+  String? phone;
+  @JsonKey(name: "link")
+  String? link;
+  @JsonKey(name: "email")
+  String? email;
+
+  ContactsResponse(this.phone, this.link, this.email);
+}
+
+@JsonSerializable()
+class AuthenticationResponse extends BaseResponse{
+  @JsonKey(name: "customer")
+  CustomerResponse? customer;
+  @JsonKey(name: "contacts")
+  ContactsResponse? contacts;
+
+  AuthenticationResponse(this.customer, this.contacts);  // from json
+
+  // to json
+}
